bda9d47fd2d34367dfdf82c0716e7fa8fdf3fecb
Denis Alekseev <52697534+Denis-Alexeev@users.noreply.github.com>
2025-10-06 12:45:25 +0200

    Update DetiFMPodcastDownloaderWithProgress.user.js

--- DetiFMPodcastDownloaderWithProgress.user.js
+++ DetiFMPodcastDownloaderWithProgress.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         DetiFM Podcast Downloader with Progress
-// @version      1.0
+// @version      1.1
 // @description  Добавляет кнопки для скачивания mp3 с прогрессом и подпапкой автора на detifm.ru
 // @match        https://detifm.ru/fairy_tales/id/*
 // @grant        GM_download
 // @run-at       document-end
 // @updateURL    https://denis-alexeev.github.io/MyUserScripts/DetiFMPodcastDownloaderWithProgress.user.js
 // @downloadURL  https://denis-alexeev.github.io/MyUserScripts/DetiFMPodcastDownloaderWithProgress.user.js
-// @homepageURL  https://github.com/Denis-Alexeev
+// @homepageURL  https://github.com/Denis-Alexeev/MyUserScripts
 // @supportURL   https://github.com/Denis-Alexeev/MyUserScripts/issues
 // ==/UserScript==
 
